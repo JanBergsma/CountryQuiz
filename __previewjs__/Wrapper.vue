4fc512290fedd05be247aa5da9d4b08473e5e1fe
JanBergsma <bergsma_jan@hotmail.com>
2023-11-24 10:51:05 +0100

    First commit

---
+++ __previewjs__/Wrapper.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import "../src/assets/main.css";
+
+// const { appContext } = getCurrentInstance();
+// appContext.app.use(mockRouter);
+</script>
+
+<template>
+    <div class="wrapper">
+        <slot />
+    </div>
+</template>
+
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Hello</h1>
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
+
