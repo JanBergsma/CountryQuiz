dd616280e3075e28cfd2f37a2f8c7462fa06ce57
JanBergsma <bergsma_jan@hotmail.com>
2023-11-27 11:37:19 +0100

    bc

--- src/views/WelcomeSreen.vue
+++ src/views/WelcomeSreen.vue
@@ -1,6 +1,13 @@
 <template>
     <section>
-        <!-- test -->
+        <div class="title">
+            <h2>Welcome</h2>
+            <h3>Where do you want to be quized about?</h3>
+        </div>
+        <div class="options">
+            <a href="capital">Quiz me about capitals.</a>
+            <a href="flag">Quiz me about flags.</a>
+        </div>
     </section>
 </template>
 
