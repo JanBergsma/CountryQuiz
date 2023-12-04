abcebd9d9d6ed767ff29ed1ba7cd9616112590d7
JanBergsma <bergsma_jan@hotmail.com>
2023-12-04 14:51:15 +0100

    bc

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/CountryQuiz/',
+  base: '/CountryQuiz/',
   plugins: [vue()],
   resolve: {
     alias: {
